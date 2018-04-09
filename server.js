77154397320f75b60038dd68219ae2d6947201cb
gabriel <gabmimouni@gmail.com>
2018-04-09 15:10:05 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+path = require('path');
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('node_modules'));
+
+
+app.listen('8000', function(){
+ console.log("yallah")
+});
